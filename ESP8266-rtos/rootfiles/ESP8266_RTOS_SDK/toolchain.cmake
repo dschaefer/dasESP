7a4ec393b146b467425458f043b8ea541a1f2b89
Doug Schaefer <dschaefer@blackberry.com>
2016-08-29 01:20:53 -0400

    Add macro for generating binaries for flashing.

--- ESP8266-rtos/rootfiles/ESP8266_RTOS_SDK/toolchain.cmake
+++ ESP8266-rtos/rootfiles/ESP8266_RTOS_SDK/toolchain.cmake
@@ -11,10 +11,14 @@ set(CMAKE_CXX_COMPILER ${gccPath}/bin/xtensa-lx106-elf-g++)
 
 set(CMAKE_C_FLAGS "-Os -g -Wpointer-arith -Wundef -Werror -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections -fdata-sections -DICACHE_FLASH" CACHE STRING "C compiler flags")
 set(CMAKE_CXX_FLAGS "-Os -g -Wpointer-arith -Wundef -Werror -Wl,-EL -fno-exceptions -fno-rtti -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -ffunction-sections -fdata-sections -DICACHE_FLASH" CACHE STRING "C++ compiler flags")
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -nostdlib -T${CMAKE_CURRENT_LIST_DIR}/ld/eagle.app.v6.ld -Wl,--no-check-sections -u call_user_start -Wl,-static" CACHE STRING "linker flags")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -nostdlib -Wl,--no-check-sections -u call_user_start -Wl,-static" CACHE STRING "linker flags")
 
 set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_C_COMPILER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -Wl,--end-group -o <TARGET>" CACHE STRING "C linker invocation")
 set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_CXX_COMPILER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> -Wl,--start-group <OBJECTS> <LINK_LIBRARIES> -Wl,--end-group -o <TARGET>" CACHE STRING "CXX linker invocation")
+
+set(ESP8266_ESPTOOL ${CMAKE_CURRENT_LIST_DIR}/tools/esptool CACHE PATH "esptool path")
+
+set(ESP8266_LD_SCRIPTS_DIR ${CMAKE_CURRENT_LIST_DIR}/ld CACHE PATH "linker script dir")
 
 include_directories(SYSTEM
 	"${CMAKE_CURRENT_LIST_DIR}/include" 
@@ -23,7 +27,7 @@ include_directories(SYSTEM
 	"${CMAKE_CURRENT_LIST_DIR}/include/lwip"
 	"${CMAKE_CURRENT_LIST_DIR}/include/lwip/ipv4"
 	"${CMAKE_CURRENT_LIST_DIR}/include/lwip/ipv6"
-	"${CMAKE_CURRENT_LIST_DIR}/nopoll"
+	"${CMAKE_CURRENT_LIST_DIR}/include/nopoll"
 	"${CMAKE_CURRENT_LIST_DIR}/include/spiffs"
 	"${CMAKE_CURRENT_LIST_DIR}/include/ssl"
 	"${CMAKE_CURRENT_LIST_DIR}/include/json" 
@@ -55,3 +59,18 @@ link_libraries(
 	-lwpa
 	-lwps
 )
+
+macro(add_esp8266_binary_target exe_target flash_size)
+	set_target_properties(${exe_target} PROPERTIES LINK_FLAGS -T${ESP8266_LD_SCRIPTS_DIR}/eagle.app.v6.ld)
+
+	add_custom_target(
+	    ${exe_target}_binary ALL
+    	COMMAND ${ESP8266_ESPTOOL} -bz ${flash_size}K
+		-eo $<TARGET_FILE:${exe_target}> -bo firmware.boot.bin -bs .text -bs .data -bs .rodata -bc -ec
+    	-eo $<TARGET_FILE:${exe_target}> -es .irom0.text firmware.irom0.txt.bin -ec
+	)
+
+	set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "firmware.boot.bin;firmware.irom0.txt.bin")
+
+	add_dependencies(${exe_target}_binary ${exe_target})
+endmacro(add_esp8266_binary_target exe_target flash_size)
